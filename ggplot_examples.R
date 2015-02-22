82139d8d0f82bd783af6ca73eb831b5a2ff9ffdf
unknown <gyz151@gmail.com>
2015-02-22 14:45:14 -0700

    Made three plots, iris, diamonds, and birthwt

--- ggplot_examples.R
+++ ggplot_examples.R
@@ -2,3 +2,49 @@
 # Zhang
 # Feb 22, 2015
 # gyz151@gmail.com
+
+install.packages("ggplot2", dependencies = TURE)
+install.packages("plyr")
+install.packages("ggthemes")
+install.packages("reshape2")
+
+library("ggplot2")
+library("reshape2")
+library("plyr")
+library("ggthemes")
+
+head(iris)
+
+myiris <- iris
+
+#data, aesthetics, geometry, stats (terminology used in the XXX library)
+
+myplot <- ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width))
+myplot + geom_point()
+
+ggplot(data = iris, aes(x = Sepal.Length, y = Sepal.Width)) +
+  geom_point(size =3)
+
+ggplot(data = iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  geom_point(size =3)
+
+ggplot(data = iris, aes(Sepal.Length, Sepal.Width, color = Species)) +
+  geom_point(aes(shape = Species), size =3)
+
+#sample a 1000 times from rows 1 to dim(diamond), and store to d2
+d2 <- diamonds[sample(1:dim(diamonds)[1], 1000), ]
+
+myplot <- ggplot(data = d2, aes(x = carat, y = price))
+
+ggplot(data = d2, aes(carat, price, color = color)) +
+  geom_point(size =3)
+
+ggplot(data = d2, aes(carat, price, color = color)) +
+  geom_point(aes(shape = color), size =3)
+
+library(MASS)
+ggplot(birthwt, aes(factor(race),bwt)) + geom_boxplot()
+
+
+
+
